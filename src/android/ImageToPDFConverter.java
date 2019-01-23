f838fec610c3b061db91af45faa6bd1b94b98705
Markanday Gupta <ixltech.markanday@gmail.com>
2019-01-23 14:10:00 +0530

    add images to pdf functionality in android app.

--- src/android/ImageToPDFConverter.java
+++ src/android/ImageToPDFConverter.java
@@ -1,32 +1,72 @@
 package com.imagetopdfconverter;
 
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
+import android.content.Context;
+import android.content.ContextWrapper;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class ImageToPDFConverter extends CordovaPlugin {
+  private File pdfFile;
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
-            return true;
-        }
-        return false;
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    if (action.equals("createPdf")) {
+      JSONArray array0 = args.getJSONArray(0);
+      String name = args.getString(1);
+      this.createPdf(array0, name, callbackContext);
+      return true;
     }
+    return false;
+  }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
+  private void createPdf(JSONArray args, String name, CallbackContext callbackContext) {
+    if (args != null && args.length() > 0) {
+      int arraySize = args.length();
+      ContextWrapper cw = new ContextWrapper(cordova.getContext());
+      File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+      try {
+        pdfFile = new File(directory, name);
+        OutputStream output = new FileOutputStream(pdfFile);
+        Document document = new Document();
+        PdfWriter.getInstance(document, output);
+        document.open();
+        for (int i = 0; i < arraySize; i++) {
+          try {
+            Image image = Image.getInstance(String.valueOf(args.get(i)));
+            image.setCompressionLevel(1);
+            image.setAlignment(Image.MIDDLE);
+            image.setBorder(Image.BOX);
+            image.scaleToFit(640f, 480f);
+            image.setBorderWidth(1);
+            document.add(image);
+            document.add(new Paragraph(""));
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
         }
+        document.close();
+        callbackContext.success(pdfFile.getPath());
+      } catch (Exception e) {
+        e.printStackTrace();
+        callbackContext.error("Error :" + e.printStackTrace());
+      }
+    } else {
+      callbackContext.error("Expected one non-empty string argument.");
     }
+  }
 }
