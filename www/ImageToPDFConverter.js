00e4190f91baf4e1449f89ddca7a5d3c2a22bed9
ixlmarkanday <ixltech.markanday@gmail.com>
2019-02-25 13:41:29 +0530

    add two optional parameters

--- www/ImageToPDFConverter.js
+++ www/ImageToPDFConverter.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.createPdf = function (arg0, arg1, success, error) {
-    exec(success, error, 'ImageToPDFConverter', 'createPdf', [arg0, arg1]);
+exports.createPdf = function (arg0, arg1, arg2 = '', arg3 = '', success, error) {
+    exec(success, error, 'ImageToPDFConverter', 'createPdf', [arg0, arg1, arg2, arg3]);
 };
